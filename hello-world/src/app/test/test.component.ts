37861fdb5c89a411cf6471bbc358a87fce595b7b
shavack <dkozuch@gmail.com>
2020-10-06 20:25:38 +0200

    ngif

--- hello-world/src/app/test/test.component.ts
+++ hello-world/src/app/test/test.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: '[app-test]',
   template: `
-    <input [(ngModel)]="name" type="text" />
-    {{ name }}
+    <button (click)="toggleGreeting()">Toggle greeting</button>
+    <h2 *ngIf="displayName; else elseBlock">{{ greetUser() }}</h2>
+    <ng-template #elseBlock>
+      <h2>No greeting</h2>
+    </ng-template>
+
+    <div *ngIf="displayName; then thenBlock1; else elseBlock1"></div>
+    <ng-template #thenBlock1>
+      <h2>Thenblock1</h2>
+    </ng-template>
+    <ng-template #elseBlock1>
+      <h2>Elseblock1</h2>
+    </ng-template>
   `,
   styles: [],
 })
 export class TestComponent implements OnInit {
-  public name = '';
+  public name = 'Dominik';
+  public displayName = true;
 
   constructor() {}
 
@@ -17,5 +29,9 @@ export class TestComponent implements OnInit {
 
   greetUser(): string {
     return 'Hello ' + this.name;
+  }
+
+  toggleGreeting(): void {
+    this.displayName = !this.displayName;
   }
 }
