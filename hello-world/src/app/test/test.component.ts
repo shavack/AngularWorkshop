4cf873311e42cc8771d4590df8890aca7417462f
shavack <dkozuch@gmail.com>
2020-10-06 19:51:50 +0200

    style binding

--- hello-world/src/app/test/test.component.ts
+++ hello-world/src/app/test/test.component.ts
@@ -3,37 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: '[app-test]',
   template: `
-    <h2 class="text-success">{{ greetUser() }}</h2>
-    <h2 [class]="successClass">{{ greetUser() }}</h2>
-    <h2 class="text-special" [class]="successClass">{{ greetUser() }}</h2>
-    <h2 [class.text-danger]="hasError">{{ greetUser() }}</h2>
-    <h2 [ngClass]="messageClasses">{{ greetUser() }}</h2>
+    <h2 [style.color]="'orange'">Style Binding</h2>
+    <h2 [style.color]="hasError ? 'red' : 'green'">Style Binding</h2>
+    <h2 [style.color]="highlightColor">Style Binding</h2>
+    <h2 [ngStyle]="titleStyles">Style Binding</h2>
   `,
-  styles: [
-    `
-      .text-success {
-        color: green;
-      }
-      .text-danger {
-        color: red;
-      }
-      .text-special {
-        font-style: italic;
-      }
-    `,
-  ],
+  styles: [],
 })
 export class TestComponent implements OnInit {
   public name = 'Dominik';
   public myId = 'testId';
   public successClass = 'text-success';
   public hasError = false;
-  public isSpecial = true;
-  
-  public messageClasses = {
-    'text-success': !this.hasError,
-    'text-danger': this.hasError,
-    'this-special': this.isSpecial,
+  public highlightColor = 'orange';
+
+  public titleStyles = {
+    color: 'blue',
+    fontStyle: 'italic',
   };
 
   constructor() {}
