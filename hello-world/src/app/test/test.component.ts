47e15c5cf553037d740600ac20fc01025ae9caee
shavack <dkozuch@gmail.com>
2020-10-06 20:41:14 +0200

    ngFor

--- hello-world/src/app/test/test.component.ts
+++ hello-world/src/app/test/test.component.ts
@@ -3,22 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: '[app-test]',
   template: `
-    <button (click)="changeColor('red')">red</button>
-    <button (click)="changeColor('green')">green</button>
-    <button (click)="changeColor('blue')">blue</button>
-    <button (click)="changeColor('none')">none</button>
-
-    <div [ngSwitch]="color">
-      <div [style.color]="color" *ngSwitchCase="'red'">You picked red</div>
-      <div [style.color]="color" *ngSwitchCase="'blue'">You picked blue</div>
-      <div [style.color]="color" *ngSwitchCase="'green'">You picked green</div>
-      <div *ngSwitchDefault>Pick again</div>
+    <div
+      *ngFor="let color of colors; index as i; last as l; odd as o; even as e"
+    >
+      <h2 [style.color]="color">
+        {{ i }}. {{ color }} last: {{ l }} odd: {{ o }} even: {{ e }}
+      </h2>
     </div>
   `,
   styles: [],
 })
 export class TestComponent implements OnInit {
   public color = 'red';
+  public colors = ['red', 'green', 'blue'];
   public name = 'Dominik';
 
   constructor() {}
