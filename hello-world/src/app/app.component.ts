a870f3bff5492c434a93a0c24baba686da17356c
shavack <dkozuch@gmail.com>
2020-10-06 21:56:16 +0200

    pipes

--- hello-world/src/app/app.component.ts
+++ hello-world/src/app/app.component.ts
@@ -7,6 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hello-world';
-  public name = 'hello from parent';
-  public message = '';
 }
--- hello-world/src/app/test/test.component.ts
+++ hello-world/src/app/test/test.component.ts
@@ -3,33 +3,34 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-    <h2>{{ name1 }}</h2>
-
-    <button (click)="fireEvent('heloooooo from child')">Fire!</button>
+    <h2>{{ name }}</h2>
+    <h2>{{ name | lowercase }}</h2>
+    <h2>{{ name | uppercase }}</h2>
+    <h2>{{ message | titlecase }}</h2>
+    <h2>{{ message | slice: 1:5 }}</h2>
+    <h2>{{ person | json }}</h2>
+    <h2>{{ stairs | number: '1.2-3' }}</h2>
+    <h2>{{ stairs | number: '3.4-5' }}</h2>
+    <h2>{{ stairs | number: '3.1-2' }}</h2>
+    <h2>{{ 0.25 | percent }}</h2>
+    <h2>{{ stairs | currency: 'GBP':'code' }}</h2>
+    <h2>{{ date | date: 'short' }}</h2>
+    <h2>{{ date | date: 'mediumDate' }}</h2>
+    <h2>{{ date | date: 'shortTime' }}</h2>
   `,
   styles: [],
 })
 export class TestComponent implements OnInit {
-  public color = 'red';
   public name = 'Dominik';
-  //@Input() public parentData;
-  @Input('parentData') name1;
-
-  @Output() public childEvent = new EventEmitter();
+  public message = 'hello there';
+  public person = {
+    firstName: 'Dominik',
+    lastName: 'Kozuch',
+  };
+  public stairs: number = 5.12345;
+  public date: Date = new Date();
 
   constructor() {}
 
   ngOnInit(): void {}
-
-  greetUser(): string {
-    return 'Hello ' + this.name;
-  }
-
-  changeColor(value: string): void {
-    this.color = value;
-  }
-
-  fireEvent(message): void {
-    this.childEvent.emit(message);
-  }
 }
