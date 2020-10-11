b746e54dbedb0ff01055daa6810c0d3cfe1f38d6
shavack <dkozuch@gmail.com>
2020-10-11 19:56:43 +0200

    goNext/goPrevious subscribing to route paramMap

--- hello-world/src/app/third/third.component.ts
+++ hello-world/src/app/third/third.component.ts
@@ -1,17 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-third',
-  template: ` <p>You selected employee {{ employeeId }}</p> `,
+  template: `
+    <p>You selected employee {{ employeeId }}</p>
+    <button (click)="goPrevious()">Previous</button>
+    <button (click)="goNext()">Next</button>
+  `,
   styles: [],
 })
 export class ThirdComponent implements OnInit {
   public employeeId;
 
-  constructor(private _route: ActivatedRoute) {}
+  constructor(private _route: ActivatedRoute, private _router: Router) {}
 
   ngOnInit(): void {
-    this.employeeId = parseInt(this._route.snapshot.paramMap.get('id'));
+    //this.employeeId = parseInt(this._route.snapshot.paramMap.get('id'));
+    this._route.paramMap.subscribe((params: ParamMap) => {
+      this.employeeId = parseInt(params.get('id'));
+    });
+  }
+
+  goPrevious(): void {
+    this._router.navigate(['/first', this.employeeId - 1]);
+  }
+
+  goNext(): void {
+    this._router.navigate(['/first', this.employeeId + 1]);
   }
 }
