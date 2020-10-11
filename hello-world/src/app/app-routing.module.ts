baddf6f95d560f2de919a30b289d5b599a5c169e
shavack <dkozuch@gmail.com>
2020-10-11 20:29:21 +0200

    component renaming

--- hello-world/src/app/app-routing.module.ts
+++ hello-world/src/app/app-routing.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { FirstComponent } from './first/first.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { SecondComponent } from './second/second.component';
-import { ThirdComponent } from './third/third.component';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
+import { EmployeeContactComponent } from './employee-contact/employee-contact.component';
+import { EmployeeOverviewComponent } from './employee-overview/employee-overview.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/first', pathMatch: 'full' },
-  { path: 'first', component: FirstComponent },
+  { path: 'first', component: EmployeeListComponent },
   { path: 'second', component: SecondComponent },
-  { path: 'first/:id', component: ThirdComponent },
+  {
+    path: 'first/:id',
+    component: EmployeeDetailComponent,
+  },
   { path: '**', component: PageNotFoundComponent },
 ];
 
@@ -20,8 +25,10 @@ const routes: Routes = [
 export class AppRoutingModule {}
 
 export const routingComponents = [
-  FirstComponent,
+  EmployeeListComponent,
   SecondComponent,
-  ThirdComponent,
+  EmployeeDetailComponent,
+  EmployeeOverviewComponent,
+  EmployeeContactComponent,
   PageNotFoundComponent,
 ];
